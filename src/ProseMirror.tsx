ponymessenger/use-prosemirror

commit 39a64506f3e9a240c34f68d760e37f7249b2ba5b

Use `EditorView#update()` instead of `#setProps()`
Per the docs, `setProps()` is equivalent to `view.update(Object.assign({}, view.props, props))`. But we want to replace the entire set of props, which is what this commit does, because that is the React way.

src/ProseMirror.tsx
@@ -31,7 +31,7 @@ export default forwardRef<Handle, Props>(function ProseMirror(
     const onChangeRef = useRef(onChange);
     onChangeRef.current = onChange;
     const viewRef = useRef<EditorView<any>>(null!);
-    viewRef.current?.setProps(props);
+    viewRef.current?.update(props);
     useEffect(() => {
         const view = new EditorView(root.current, {
             ...initialProps.current,

commit 76877f8748b23961aaf0622a9395987f19fdee76

Fixed bug introduced in 39a6450
`EditorView.update()` replaces all props, so `dispatchTransaction` needs
to be set every time props are updated.

src/ProseMirror.tsx
@@ -31,14 +31,12 @@ export default forwardRef<Handle, Props>(function ProseMirror(
     const onChangeRef = useRef(onChange);
     onChangeRef.current = onChange;
     const viewRef = useRef<EditorView<any>>(null!);
-    viewRef.current?.update(props);
+    viewRef.current?.update(buildProps(props));
     useEffect(() => {
-        const view = new EditorView(root.current, {
-            ...initialProps.current,
-            dispatchTransaction: transaction => {
-                onChangeRef.current(view.state.apply(transaction));
-            },
-        });
+        const view = new EditorView(
+            root.current,
+            buildProps(initialProps.current),
+        );
         viewRef.current = view;
         return () => {
             view.destroy();
@@ -50,4 +48,14 @@ export default forwardRef<Handle, Props>(function ProseMirror(
         },
     }));
     return <div ref={root} style={style} className={className} />;
+    function buildProps(props: DirectEditorProps): DirectEditorProps {
+        return {
+            ...props,
+            dispatchTransaction: transaction => {
+                onChangeRef.current(
+                    viewRef.current.state.apply(transaction),
+                );
+            },
+        };
+    }
 });