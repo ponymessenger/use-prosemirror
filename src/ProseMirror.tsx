3b5f337f6b1b0a89b99b01bb6e162d754696e7bb
Dmitry Minkovsky <dminkovsky@gmail.com>
2020-10-20 08:10:28 -0400

    Fix #2: Scroll into view is broken

--- src/ProseMirror.tsx
+++ src/ProseMirror.tsx
@@ -44,21 +44,25 @@ export default forwardRef<Handle, Props>(function ProseMirror(
 ): JSX.Element {
     const root = useRef<HTMLDivElement>(null!);
     const initialProps = useRef(props);
-    const buildPropsRef = useRef(buildProps);
-    buildPropsRef.current = buildProps;
     const viewRef = useRef<EditorView<any>>(null!);
-    // If this is a non-initial render, update the editor view as part
-    // of the React render. Otherwise, bootstrap and render in
-    // `useEffect` below.
-    viewRef.current?.update(buildProps(props));
+    // If this is a non-initial render, update the editor view with
+    // the React render.
+    // - First update editor state using `EditorView#updateState()`.
+    // - Then update other props using `EditorView#setProps()`.
+    // If we update state with other props together using
+    // `setProps()`, scroll-into-view will not occur due to:
+    // https://github.com/ProseMirror/prosemirror-view/blob/13b046a834b489530a98dd362fa55703e52e076d/src/index.js#L183-L195
+    const {state, ...restProps} = props;
+    viewRef.current?.updateState(state);
+    viewRef.current?.setProps(buildProps(restProps));
     useEffect(() => {
         // Bootstrap the editor on first render. Note: running
         // non-initial renders inside `useEffect` produced glitchy
         // behavior.
-        const view = new EditorView(
-            root.current,
-            buildPropsRef.current(initialProps.current),
-        );
+        const view = new EditorView(root.current, {
+            state: initialProps.current.state,
+            ...buildProps(initialProps.current),
+        });
         viewRef.current = view;
         return () => {
             view.destroy();
@@ -77,14 +81,16 @@ export default forwardRef<Handle, Props>(function ProseMirror(
         />
     );
 
-    function buildProps(props: Props): DirectEditorProps {
+    function buildProps(
+        props: Partial<Props>,
+    ): Partial<DirectEditorProps> {
         return {
             ...props,
             dispatchTransaction: transaction => {
                 // `dispatchTransaction` takes precedence.
                 if (props.dispatchTransaction) {
                     props.dispatchTransaction(transaction);
-                } else {
+                } else if (props.onChange) {
                     props.onChange(
                         viewRef.current.state.apply(transaction),
                     );
